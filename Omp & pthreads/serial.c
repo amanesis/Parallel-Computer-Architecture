7f99fe80e83daf8783f2e0a7d8ad9f64f7c8a14e
amanesis <amanesis_tuc@outlook.com>
2019-03-25 04:46:34 +0200

    working

--- Omp & pthreads/serial.c
+++ Omp & pthreads/serial.c
@@ -15,11 +15,8 @@
 #define LEFT 2
 #define DIAGONAL 3
 
-#define RESET   "\033[0m"
-#define BOLDRED "\033[1m\033[31m"      /* Bold Red */
-
-long long int m = 0;
-long long int n = 0;
+long long int m = 1;
+long long int n = 1;
 
 const char* name_flag = "-name";
 const char* input_flag = "-input";
@@ -33,6 +30,9 @@ char* d_seq;
 long int match;
 long int mismatch;
 long int gap;
+int count_match=0;
+int count_cells=0;
+int count_traceback=0;
 
 double gettime(void)
 {
@@ -75,7 +75,7 @@ int open_file(char* input, char* name, FILE* *inputfile, FILE* *outputfile)
 {
     char cwd[256];
     getcwd(cwd, sizeof(cwd));
-    printf("%s\n", cwd);
+    //printf("%s\n", cwd);
     char* ftp = concat(4, cwd, "/", input, ".txt"); //Path to input file
     *inputfile = fopen(ftp, "r");
     char* outfile = concat(4, cwd, "/Report_", name, ".txt");
@@ -120,7 +120,7 @@ void printPredecessor(int* matrix)
                 else if (matrix[index] == -LEFT)
                     printf("< ");
                 else if (matrix[index] == -DIAGONAL)
-                    printf("|^ ");
+                    printf("|< ");
                 else
                     printf("- ");
             } else
@@ -147,28 +147,28 @@ int parsingInfo(FILE* inputfile, int *pair_size, long int *q_size, long int *q_s
 	{
         sscanf(line,"Pairs:\t%[^\n]",line); //reads formatted input from a string.
         *pair_size = atoi(line);
-        printf("Pairs: %d\n" , *pair_size);
+      //  printf("Pairs: %d\n" , *pair_size);
     }
 
     if (fgets(line, line_size, inputfile))
 	{
         sscanf(line,"Q_Sz_Min:\t%[^\n]", line);
         *q_size_min = atoi(line);
-        printf("Q size (min): %d\n", *q_size_min);
+        //printf("Q size (min): %d\n", *q_size_min);
     }
 
     if (fgets(line, line_size, inputfile))
 	{
         sscanf(line,"Q_Sz_Max:\t%[^\n]", line);
         *q_size = atoi(line);
-        printf("Q size: %d\n", *q_size);
+      //  printf("Q size: %d\n", *q_size);
     }
 
     if (fgets(line, line_size, inputfile))
 	{
         sscanf(line,"D_Sz_All:\t%[^\n]", line);
         *d_size = atoi(line);
-        printf("D size: %d\n", *d_size);
+      //  printf("D size: %d\n", *d_size);
     }
     else
 	{
@@ -191,17 +191,16 @@ int parsing(FILE* inputfile, FILE* outputfile, char *q_seq, char *d_seq)
 		if (strncmp(line, "Q:", 2) == 0)
 		{
     		sscanf(line, "Q:\t%[^\n]", line);
-            fprintf(outputfile, "Q:\t%s\n", line);
-			strcpy(q_seq, line);
-
-			found_q = 1;
+        fprintf(outputfile, "Q:\t%s\n", line);
+				strcpy(q_seq, line);
+				found_q = 1;
 		}
 		else if (strncmp(line, "D:", 2) == 0)
 		{
-			sscanf(line, "D:\t%[^\n]", line);
-            fprintf(outputfile, "D:\t%s\n", line);
-			strcpy(d_seq, line);
-			found_q = 0;
+					sscanf(line, "D:\t%[^\n]", line);
+          fprintf(outputfile, "D:\t%s\n", line);
+					strcpy(d_seq, line);
+					found_q = 0;
 		}
 		else if (strncmp(line, "\t", 1) == 0 || strncmp(line, "  ", 2) == 0)
 		{
@@ -232,16 +231,19 @@ void Score(long int i, long int j, int *H,  int *P, int* maxPos)
 	{
         max = diag;
         pred = DIAGONAL;
+		count_cells++;
 	}
     if (up > max)
 	{
         max = up;
         pred = UP;
+		count_cells++;
     }
     if (left > max)
 	{
         max = left;
         pred = LEFT;
+		count_cells++;
     }
 
     H[index] = max;
@@ -254,12 +256,13 @@ int matchMissmatch(long int i, long int j)
 {
     if (q_seq[j-1] == d_seq[i-1])
     {
-    	printf("OP Match: %d\n", match);
+    	//printf("OP Match: %d\n", match);
+		count_match++;
         return match; //match
     }
     else
     {
-    	printf("OP MisMatch: %d\n", mismatch);
+    	//printf("OP MisMatch: %d\n", mismatch);
         return mismatch; //MisMatch
     }
 }
@@ -270,12 +273,15 @@ void backtrack(int* P, long long int maxPos)
 
     //backtrack from maxPos to startPos = 0
     do {
-        if(P[maxPos] == DIAGONAL)
+        if(P[maxPos] == DIAGONAL){
             predPos = maxPos - m - 1;
-        else if(P[maxPos] == UP)
+			count_traceback++;}
+        else if(P[maxPos] == UP){
             predPos = maxPos - m;
-        else if(P[maxPos] == LEFT)
+			count_traceback++;}
+        else if(P[maxPos] == LEFT){
             predPos = maxPos - 1;
+			count_traceback++;}
         P[maxPos]*=PATH;
         maxPos = predPos;
     } while(P[maxPos] != NONE);
@@ -284,8 +290,9 @@ void backtrack(int* P, long long int maxPos)
 
 int main(int argc, char* argv[])
 {
-    char* name ="nametest0";
-    char* input = "D0";
+	double begin = gettime();
+    char* name ="nametest2";
+    char* input = "D2";
     int pair_size = 0;
     long int q_size = 0;
     long int q_size_min = 0;
@@ -298,7 +305,7 @@ int main(int argc, char* argv[])
     gap = -4;
 
     FILE* inputfile;
-	FILE* outputfile;
+		FILE* outputfile;
     open_file(input,name,&inputfile,&outputfile);
 
     parsingInfo(inputfile, &pair_size, &q_size, &q_size_min, &d_size);
@@ -308,8 +315,8 @@ int main(int argc, char* argv[])
 
     parsing(inputfile, outputfile, q_seq, d_seq);
 
-    printf("%s\n\n", q_seq);
-    printf("%s\n\n", d_seq);
+    //printf("%s\n\n", q_seq);
+    //printf("%s\n\n", d_seq);
 
     m = strlen(q_seq);
     n = strlen(d_seq);
@@ -335,14 +342,38 @@ int main(int argc, char* argv[])
   		}
    	}
 
+		printf("A) Q-D matches: %d.\n", count_match);
+		printf("B) Total cells with new value: %d.\n", count_cells);
+
+		double traceback_begin = gettime();
    	backtrack(P, maxPos);
+		double traceback_total = gettime()-traceback_begin;
+
+		printf("C) Traceback Steps: %d.\n", count_traceback );
 
    	double time1 = gettime();
    	elapsed = time1 - time0;
-   	printf("Time: %f sec.\n", elapsed);
 
-	printMatrix(H);
-	printPredecessor(P);
+
+
+
+
+		fclose(inputfile);
+    fclose(outputfile);
+
+		printf("D) Total execution time: %f sec.\n", gettime()-begin);
+		printf("E) Total time for cells calculation: %f sec.\n", elapsed);
+		printf("F) Traceback time: %f sec.\n", traceback_total);
+		printf("G) CUPS based on total execution time: %f cells/sec.\n", count_cells/(gettime()-begin));
+		printf("H) CUPS based on calculation time: %f cells/sec.\n\n\n", count_cells/elapsed);
+
+
+		/*printf("Some extras: \n\n");
+		printf("Scoring Matrix: \n");
+		printMatrix(H);
+		printf("\n");
+		printf("Traceback Matrix: \n");
+		printPredecessor(P);*/
 
     free(H);
     free(P);
@@ -350,7 +381,6 @@ int main(int argc, char* argv[])
     free(q_seq);
     free(d_seq);
 
-	fclose(inputfile);
-    fclose(outputfile);
+
     return 0;
 }
